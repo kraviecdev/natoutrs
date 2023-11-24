95f4833658a2540b5b573b766090b927783d0015
patrick36212 <patrick36212@gmail.com>
2023-11-24 18:49:56 +0100

    add duplicate fields errors & validation errors to prod env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,20 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidatorErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((value) => value.message);
+
+  const message = `Invalid input data: ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -41,6 +55,14 @@ module.exports = (err, req, res, next) => {
 
     if (err.name === "CastError") {
       error = handleCastErrorDB(error);
+    }
+
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    }
+
+    if (err.name === "ValidationError") {
+      error = handleValidatorErrorDB(error);
     }
 
     sendErrorProd(error, res);
