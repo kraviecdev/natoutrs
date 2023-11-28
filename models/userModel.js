3785996fef7a8381ba04eae18c145a71441d600e
patrick36212 <patrick36212@gmail.com>
2023-11-28 13:43:20 +0100

    add pre-save middleware for hashing with bcrypt passwords before save in DB

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please tell us your name!"],
     maxlength: [30, "Max username length is 30"],
-    minlength: [5, "Min username length is 5"],
   },
   email: {
     type: String,
@@ -33,6 +33,15 @@ const userSchema = new mongoose.Schema({
       message: "Passwords must be the same",
     },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
