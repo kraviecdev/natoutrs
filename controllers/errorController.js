7a39e4c4f0706a515e642aca21a059ac1d371a1b
patrick36212 <patrick36212@gmail.com>
2023-12-03 18:39:48 +0100

    add error handlers for JWT

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,6 +21,12 @@ const handleValidatorErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again!", 401);
+
+const handleJWTExpiredError = () =>
+  new AppError("Your token has expired! Please log in again.", 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -54,17 +60,24 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
-
-    if (err.name === "CastError") {
+    if (error.name === "CastError") {
       error = handleCastErrorDB(error);
     }
 
-    if (err.code === 11000) {
-      error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError") {
+      error = handleValidatorErrorDB(error);
     }
 
-    if (err.name === "ValidationError") {
-      error = handleValidatorErrorDB(error);
+    if (error.name === "JsonWebTokenError") {
+      error = handleJWTError();
+    }
+
+    if (error.name === "TokenExpiredError") {
+      error = handleJWTExpiredError();
+    }
+
+    if (error.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
     }
 
     sendErrorProd(error, res);
