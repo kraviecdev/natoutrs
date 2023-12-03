6111b6676ad6f2f4482537c0d3f38f103956edce
patrick36212 <patrick36212@gmail.com>
2023-12-03 19:16:32 +0100

    create protect middleware for get all tours path, add token validators

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,13 @@
 const jwt = require("jsonwebtoken");
-
+const { promisify } = require("util");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-const signToken = (id) => {
+const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-};
 
 exports.singup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -47,6 +46,29 @@ exports.login = catchAsync(async (req, res, next) => {
   const token = signToken(user._id);
   res.status(200).json({
     status: "success",
-    token,
+    token: token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401),
+    );
+  }
+
+  //checking token validation
+  await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  next();
 });
