7c046a06da2ff72d1cc0ed7bf8320ddef3455b21
patrick36212 <patrick36212@gmail.com>
2023-12-04 16:26:36 +0100

    fix limit props

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -45,8 +45,8 @@ class ApiFeatures {
   }
 
   paginate() {
-    const page = this.queryString * 1 || 1;
-    const limit = this.queryString * 1 || 100;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
