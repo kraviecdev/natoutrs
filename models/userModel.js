331c5ab6de1bb6bbf6c250b3d4f326c822f984e0
patrick36212 <patrick36212@gmail.com>
2023-12-07 17:59:32 +0100

    add user role to userSchema. Make it not selective in searches

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,12 @@ const userSchema = new mongoose.Schema({
   photo: {
     type: String,
   },
+  role: {
+    type: String,
+    enum: ["user", "guide", "lead-guide", "admin"],
+    default: "user",
+    select: false,
+  },
   password: {
     type: String,
     required: [true, "Password is required"],
