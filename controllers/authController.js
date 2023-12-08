81b716cbd000d0d9b52d7c356c2c37952228ea84
patrick36212 <patrick36212@gmail.com>
2023-12-08 19:34:32 +0100

    create forgot pass controller with sending random token link as response

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { promisify } = require("util");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -101,3 +102,49 @@ exports.restrictTo =
 
     next();
   };
+
+exports.forgotPass = catchAsync(async (req, res, next) => {
+  //get user from post,
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new AppError("There is no user with this email address", 404));
+  }
+  //generate random token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  //send token to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    "host",
+  )}/api/v1/users/resetPass/${resetToken}`;
+
+  const message = `Forgot your password, submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
+    await sendEmail({
+      email: req.body.email,
+      subject: "Your Password token (valid for 10 min)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        "There was an error with sending this email, try again later",
+        500,
+      ),
+    );
+  }
+});
+
+exports.resetPass = (req, res, next) => {};
