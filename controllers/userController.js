9026a1e7d1ddf9b5e6b9f7d0dfc64584974b1a1f
patrick36212 <patrick36212@gmail.com>
2023-12-11 17:56:04 +0100

    create user Controller, controller for updateMyData & getAllUsers. Add res 500 for the admin CRUD controllers

---
+++ controllers/userController.js
@@ -0,0 +1,79 @@
+const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //error if the user post password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use correct one",
+        400,
+      ),
+    );
+  }
+  //filter for only allowed fieldNames
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  //update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "this route is not yet defined!",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "this route is not yet defined!",
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "this route is not yet defined!",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "this route is not yet defined!",
+  });
+};
