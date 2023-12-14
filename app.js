5c90dae6f00159d6ee49c420fff97a55c823c59e
patrick36212 <patrick36212@gmail.com>
2023-12-14 17:42:43 +0100

    add mongo sanitization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -29,6 +30,9 @@ app.use("/api", limiter);
 
 //Body parser, reads data from req.body
 app.use(express.json({ limit: "10kb" }));
+
+//Data sanitization (NoSQL query injection)
+app.use(mongoSanitize());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
