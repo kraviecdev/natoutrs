ede6cae99a34da0d04721b8927016f5627fe348a
patrick36212 <patrick36212@gmail.com>
2023-12-14 17:55:56 +0100

    implement hpp with whitelist opt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -33,6 +34,22 @@ app.use(express.json({ limit: "10kb" }));
 
 //Data sanitization (NoSQL query injection)
 app.use(mongoSanitize());
+
+//Data sanitization (XSS)
+
+//Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "maxGroupSize",
+      "price",
+      "difficulty",
+    ],
+  }),
+);
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
