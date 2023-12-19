89258e5758609a9a777eed0293796cd7ddc3290e
patrick36212 <patrick36212@gmail.com>
2023-12-19 18:13:31 +0100

    create reviewController with createReview & getAllReview controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,27 @@
+const Review = require("../models/reviewModel");
+const catchAsync = require("../utils/catchAsync");
+// const AppError = require("../utils/appError");
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const review = await Review.create(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      review,
+    },
+  });
+});
