4f7cd9c8e5ffee36b6faccca35293fba147a767b
patrick36212 <patrick36212@gmail.com>
2023-12-20 13:26:56 +0100

    add code to filter reviews by tourId and add reviews to specified tourId

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,7 +3,10 @@ const catchAsync = require("../utils/catchAsync");
 // const AppError = require("../utils/appError");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: "success",
@@ -16,6 +19,9 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const review = await Review.create(req.body);
 
   res.status(200).json({
