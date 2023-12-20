bed86b48a840d3c5f6cdfa090e02ae3c3a93ed8f
patrick36212 <patrick36212@gmail.com>
2023-12-20 16:34:15 +0100

    implement getOne & getAll handlers

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,4 @@
 const Review = require("../models/reviewModel");
-const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
 
 exports.setTourUserIds = (req, res, next) => {
@@ -8,24 +7,8 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
 exports.createReview = factory.createOne(Review);
-
+exports.getReview = factory.getOne(Review);
+exports.getAllReviews = factory.getAll(Review);
 exports.updateReview = factory.updateOne(Review);
-
 exports.deleteReview = factory.deleteOne(Review);
