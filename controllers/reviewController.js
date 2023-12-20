fc932f97d474a84d23ee0c6595b30fae94669646
patrick36212 <patrick36212@gmail.com>
2023-12-20 15:04:57 +0100

    implement updateOne & createOne in factoryHandler. Create setTourUserIds middleware

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,6 +2,12 @@ const Review = require("../models/reviewModel");
 const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
 
+exports.setTourUserIds = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
@@ -18,18 +24,8 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+exports.createReview = factory.createOne(Review);
 
-  const review = await Review.create(req.body);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      review,
-    },
-  });
-});
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
