fdd27bb72a1aeafd99e9ec2f71a52523c77982a6
patrick36212 <patrick36212@gmail.com>
2023-12-23 10:23:58 +0100

    delete pre aggregation to make $geoNear works

--- models/tourModel.js
+++ models/tourModel.js
@@ -149,11 +149,11 @@ tourSchema.pre(/^find/, function (next) {
 
   next();
 });
-// AGGREGATION MIDDLEWARE
-tourSchema.pre("aggregate", function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  next();
-});
+// // AGGREGATION MIDDLEWARE
+// tourSchema.pre("aggregate", function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
