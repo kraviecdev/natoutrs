b581d6e96a81fc2711b87ec37b54cfe97ef2f447
patrick36212 <patrick36212@gmail.com>
2023-12-29 12:44:16 +0100

    create controllers for views

---
+++ controllers/viewController.js
@@ -0,0 +1,27 @@
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // Get tour data from collection
+  const tours = await Tour.find();
+  // Build template
+  // Render that template using data
+  res.status(200).render("overview", {
+    tours,
+  });
+});
+
+exports.getTourView = catchAsync(async (req, res) => {
+  // Get data for the requested tour (including guides and reviews)
+
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "user review rating photo",
+  });
+  // Build Template
+
+  // Render template using data
+  res.status(200).render("tour", {
+    tour,
+  });
+});
