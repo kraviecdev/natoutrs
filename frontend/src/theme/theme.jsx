6bc7c6378e99ee598a07f3adfecd8f0134259dee
patrick36212 <patrick36212@gmail.com>
2024-01-02 20:57:10 +0100

    create theme object for styled components

---
+++ frontend/src/theme/theme.jsx
@@ -0,0 +1,37 @@
+export const theme = {
+  breakpoints: {
+    sm: 596,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+  colors: {
+    indigo: "#34413A",
+    green: "#006732",
+    gray: "#5F766A",
+    white: "#FFFFFF",
+    beige: "#E7F2ED",
+    shadow: "#00000040",
+    invalid: "#ab4949",
+  },
+  font: {
+    family: {
+      main: "Lato",
+      secondary: "Playfair Display",
+    },
+    size: {
+      small: 14,
+      regular: 16,
+      medium: 24,
+      extraMedium: 32,
+      large: 40,
+      extraLarge: 64,
+    },
+    weight: {
+      regular: 400,
+      medium: 500,
+      semiBold: 700,
+      bold: 900,
+    },
+  },
+};
