73a2a6577afef7788c328825755957b253c548fa
patrick36212 <patrick36212@gmail.com>
2024-01-03 11:18:27 +0100

    delete view controller and routes

--- server/routes/viewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getOverview, getTourView } = require("../controllers/viewController");
-
-const router = express.Router();
-
-router.get("/", getOverview);
-router.get("/tour/:slug", getTourView);
-
-module.exports = router;
