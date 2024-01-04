16a6727c952568fb1b2e64f328711ff99fff828e
patrick36212 <patrick36212@gmail.com>
2024-01-04 11:59:37 +0100

    create Map component with mapbox

---
+++ frontend/src/components/Map/index.jsx
@@ -0,0 +1,57 @@
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect } from "react";
+
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1Ijoia3JhdmllY2RldiIsImEiOiJjbHFxbjBqamEzbTJvMmlvMG41dms1Mm96In0.esw8Ems3QLF6tFeTxtPSOg";
+
+const Map = ({ locations }) => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/kraviecdev/clqqn5l9400w201pjc4measvn",
+      accessToken: MAPBOX_ACCESS_TOKEN,
+    });
+
+    const bounds = new mapboxgl.LngLatBounds();
+
+    locations.forEach((loc) => {
+      const marker = document.createElement("div");
+      marker.className = "marker";
+
+      // Add marker
+      new mapboxgl.Marker({
+        element: marker,
+        anchor: "bottom",
+      })
+        .setLngLat(loc.coordinates)
+        .addTo(map);
+
+      // Add popup
+      new mapboxgl.Popup({
+        offset: 30,
+      })
+        .setLngLat(loc.coordinates)
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map);
+
+      // Extend map bounds to include current location
+      bounds.extend(loc.coordinates);
+    });
+
+    map.fitBounds(bounds, {
+      padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100,
+      },
+    });
+
+    return () => map.remove();
+  }, []);
+
+  return <div id="map"></div>;
+};
+
+export default Map;
