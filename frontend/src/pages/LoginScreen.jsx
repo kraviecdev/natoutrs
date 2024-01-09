8d88f8cbcaff9565e5579fb2e6795f5ea761fe06
patrick36212 <patrick36212@gmail.com>
2024-01-09 18:48:45 +0100

    create basic login screen

---
+++ frontend/src/pages/LoginScreen.jsx
@@ -0,0 +1,60 @@
+import { Form, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch.js";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/users/login", data);
+    return redirect("/");
+  } catch (error) {
+    console.error(error.statusText);
+    return error;
+  }
+};
+const LoginScreen = () => {
+  return (
+    <main className="main">
+      <div className="login-form">
+        <h2 className="heading-secondary ma-bt-lg">Log into your account</h2>
+        <Form method="post" className="form">
+          <div className="form__group">
+            <label htmlFor="email" className="form__label">
+              Email address
+            </label>
+            <input
+              className="form__input"
+              placeholder="you@example.com"
+              required
+              id="email"
+              name="email"
+              type="email"
+            />
+          </div>
+
+          <div className="form__group ma-bt-md">
+            <label htmlFor="password" className="form__label">
+              Password
+            </label>
+            <input
+              className="form__input"
+              placeholder="••••••••"
+              required
+              minLength="8"
+              id="password"
+              name="password"
+              type="password"
+            />
+          </div>
+
+          <div className="form__group">
+            <button className="btn btn--green">Login</button>
+          </div>
+        </Form>
+      </div>
+    </main>
+  );
+};
+
+export default LoginScreen;
