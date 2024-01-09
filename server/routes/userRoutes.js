975aba1ab5c62cd0da7582338aedcf51300c18d9
patrick36212 <patrick36212@gmail.com>
2024-01-09 18:48:24 +0100

    add logout route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/signup", authController.singup);
 router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 router.post("/forgotPass", authController.forgotPass);
 router.patch("/resetPass/:token", authController.resetPass);
 
