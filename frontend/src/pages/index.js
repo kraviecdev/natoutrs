0ba50077a1ea73fd36083601345772ade9e77eee
patrick36212 <patrick36212@gmail.com>
2024-01-09 18:49:13 +0100

    export as default login screen

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as HomeLayout } from "./HomeLayout.jsx";
 export { default as Overview } from "./Overview.jsx";
 export { default as TourDetails } from "./TourDetails.jsx";
+export { default as LoginScreen } from "./LoginScreen.jsx";
