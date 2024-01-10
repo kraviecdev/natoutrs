68ca27a7f404d3d1fc23f4e4259d08bbd9dadb4b
patrick36212 <patrick36212@gmail.com>
2024-01-10 14:05:06 +0100

    add loader to home, checking users

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import Footer from "../components/Footer/index.jsx";
 import Header from "../components/Header/index.jsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
+import customFetch from "../utils/customFetch.js";
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get("/users/me");
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
 
 const Home = () => {
+  const { data } = useLoaderData();
   return (
     <>
-      <Header />
+      <Header user={data} />
       <Outlet />
       <Footer />
     </>
