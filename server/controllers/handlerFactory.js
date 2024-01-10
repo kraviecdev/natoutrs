4513ba87e67f2e04b4fa2967235296689570521f
patrick36212 <patrick36212@gmail.com>
2024-01-10 12:03:51 +0100

    refactors docs to data

--- server/controllers/handlerFactory.js
+++ server/controllers/handlerFactory.js
@@ -4,13 +4,11 @@ const APIFeatures = require("../utils/apiFeatures");
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const newDoc = await Model.create(req.body);
+    const data = await Model.create(req.body);
 
     res.status(201).json({
       status: "success",
-      data: {
-        data: newDoc,
-      },
+      data,
     });
   });
 
@@ -19,15 +17,15 @@ exports.getOne = (Model, popOpt) =>
     let query = Model.findById(req.params.id);
     if (popOpt) query = query.populate(popOpt);
 
-    const doc = await query;
+    const data = await query;
 
-    if (!doc) {
+    if (!data) {
       return next(new AppError("Cannot find that ID", 404));
     }
 
     res.status(200).json({
       status: "success",
-      data: { data: doc },
+      data,
     });
   });
 
@@ -41,40 +39,38 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const docs = await features.query;
+    const data = await features.query;
 
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
-      results: docs.length,
-      data: { data: docs },
+      results: data.length,
+      data,
     });
   });
 
 exports.updateOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+    const data = await Model.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
 
-    if (!doc) {
+    if (!data) {
       return next(new AppError("Cannot find that ID", 404));
     }
 
     res.status(200).json({
       status: "success",
-      data: {
-        data: doc,
-      },
+      data,
     });
   });
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndDelete(req.params.id);
+    const data = await Model.findByIdAndDelete(req.params.id);
 
-    if (!doc) {
+    if (!data) {
       return next(new AppError("Cannot find that ID", 404));
     }
 
