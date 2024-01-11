c1ca73b63a973ab02c1f45ce5f4df6a40e46b39d
patrick36212 <patrick36212@gmail.com>
2024-01-11 18:39:28 +0100

    transform header to styled component

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
-import customFetch from "../../utils/customFetch.js";
 import { useEffect, useState } from "react";
+import customFetch from "../../utils/customFetch.js";
+import { Nav } from "../Navigation/styled.js";
+import { Img } from "../Img/styled.js";
+import { StyledLink } from "../Link/styled.js";
+import Wrapper from "../Wrapper/styled.js";
 
 const Header = ({ user }) => {
   const [isLogged, setIsLogged] = useState(false);
@@ -16,41 +19,39 @@ const Header = ({ user }) => {
   };
 
   return (
-    <header className="header">
-      <nav className="nav nav--tours">
-        <Link href="/" className="nav__el">
-          All tours
-        </Link>
-      </nav>
-      <div className="header__logo">
-        <img src="/img/logo-white.png" alt="Natours logo" />
-      </div>
-      <nav className="nav nav--user">
-        {isLogged ? (
-          <>
-            <button onClick={() => logout()} className="nav__el">
-              Log out
-            </button>
-            <Link className="nav__el" to="/me">
-              <img
-                className="nav__user-img"
-                src={`/img/users/${user.photo}`}
-                alt={`Photo of ${user.name}`}
-              />
-              <span>{user.name.split(" ")[0]}</span>
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link className="nav__el" to="/login">
-              Log in
-            </Link>
-            <Link className="nav__el nav__el--cta" to="/signup">
-              Sign up
-            </Link>
-          </>
-        )}
-      </nav>
+    <header>
+      <Nav>
+        <StyledLink href="/">
+          <Img src="/img/logo-white.png" alt="Natours logo" />
+          <span>All tours</span>
+        </StyledLink>
+
+        <Wrapper>
+          {isLogged ? (
+            <>
+              <StyledLink to="/" onClick={() => logout()}>
+                Log out
+              </StyledLink>
+              <StyledLink to="/me">
+                <Img
+                  user
+                  round
+                  src={`/img/users/user-1.jpg`}
+                  alt={`Photo of ${user.name}`}
+                />
+                <span>{user.name.split(" ")[0]}</span>
+              </StyledLink>
+            </>
+          ) : (
+            <>
+              <StyledLink to="/login">Log in</StyledLink>
+              <StyledLink cta to="/signup">
+                Sign up
+              </StyledLink>
+            </>
+          )}
+        </Wrapper>
+      </Nav>
     </header>
   );
 };
