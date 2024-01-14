7aa8e1a6063cfd2ecb6d48bedf0c585ae5b08694
patrick36212 <patrick36212@gmail.com>
2024-01-14 16:47:45 +0100

    implement new color names

--- frontend/src/components/Link/styled.js
+++ frontend/src/components/Link/styled.js
@@ -7,7 +7,7 @@ export const StyledLink = styled(NavLink)`
   justify-content: flex-start;
   align-items: center;
   text-decoration: none;
-  color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.second_font};
   text-transform: uppercase;
   transition: all 0.4s ease-in-out;
   font-size: ${({ theme }) => theme.font.size.small}px;
@@ -37,8 +37,7 @@ export const StyledLink = styled(NavLink)`
       padding: 12px 24px;
 
       &:hover {
-        background: ${({ theme }) => theme.colors.white};
-        color: ${({ theme }) => theme.colors.indigo};
+        background: ${({ theme }) => theme.colors.contrast};
       }
     `}
 `;
