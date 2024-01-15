b795cd29ecc6d294cc46c8d2751746736aacf75e
patrick36212 <patrick36212@gmail.com>
2024-01-15 15:20:57 +0100

    add selection styles, create styled for main

--- frontend/src/theme/GlobalStyle.jsx
+++ frontend/src/theme/GlobalStyle.jsx
@@ -13,7 +13,9 @@ export const GlobalStyle = createGlobalStyle`
     body {
         display: flex;
         flex-direction: column;
+        align-items: center;
         font-family: Lato, sans-serif;
+        gap: 32px;
         font-size: ${({ theme }) => theme.font.size.regular}px;
         font-weight: ${({ theme }) => theme.font.weight.regular};
         line-height: normal;
@@ -21,9 +23,34 @@ export const GlobalStyle = createGlobalStyle`
         color: ${({ theme }) => theme.colors.main_font};
         min-height: 100vh;
     }
-    
-    header { 
+
+    ::-moz-selection {
+        background: ${({ theme }) => theme.colors.contrast};
+        color: ${({ theme }) => theme.colors.second_font};
+    }
+
+    ::selection {
+        background: ${({ theme }) => theme.colors.contrast};
+        color: ${({ theme }) => theme.colors.second_font};
+    }
+
+    header {
         width: 100%;
         background: ${({ theme }) => theme.colors.second};
+    }
+
+    main {
+        max-width: ${({ theme }) => theme.breakpoints.xl}px;
+        width: 100%;
+        display: grid;
+        justify-items: center;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        grid-gap: 24px;
+        padding: 0 10px;
+
+        @media only screen and (min-width: ${({ theme }) =>
+          theme.breakpoints.xl}px) {
+            padding: 0;
+        }
     }
 `;
