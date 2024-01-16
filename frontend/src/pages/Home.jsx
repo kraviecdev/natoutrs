e64e3faf83da2bdc0c6146ef1a2708f570fb4b12
patrick36212 <patrick36212@gmail.com>
2024-01-16 18:20:58 +0100

    move logout fn to Home dir

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Footer from "../components/Footer/index.jsx";
 import Header from "../components/Header/index.jsx";
 import { Outlet, useLoaderData } from "react-router-dom";
 import customFetch from "../utils/customFetch.js";
+import { toast } from "react-toastify";
 
 export const loader = async () => {
   try {
@@ -14,9 +15,16 @@ export const loader = async () => {
 
 const Home = () => {
   const { data } = useLoaderData();
+
+  const logout = async () => {
+    await customFetch.get("/users/logout");
+    toast.success("Logged out.");
+    window.location.reload();
+  };
+
   return (
     <>
-      <Header user={data} />
+      <Header user={data} logout={logout} />
       <Outlet />
       <Footer />
     </>
