998ee4e2d60e7e509b6357a972dd5f3f77576227
patrick36212 <patrick36212@gmail.com>
2024-01-22 18:00:41 +0100

    export sign up component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home.jsx";
 export { default as Overview } from "./Overview.jsx";
 export { default as Tour } from "./Tour.jsx";
 export { default as Login } from "./Login.jsx";
+export { default as Signup } from "./Signup.jsx";
