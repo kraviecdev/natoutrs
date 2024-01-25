e0e753477b5477fc8206d6f0e168a3872b29a291
patrick36212 <patrick36212@gmail.com>
2024-01-25 17:46:15 +0100

    add media to adjust svg size

--- frontend/src/components/Icon/styled.js
+++ frontend/src/components/Icon/styled.js
@@ -4,4 +4,9 @@ export const StyledIcon = styled.svg`
   width: 24px;
   height: 24px;
   fill: ${({ theme }) => theme.colors.contrast};
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
+    width: 32px;
+    height: 32px;
+  }
 `;
