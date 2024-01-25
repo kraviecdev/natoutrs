aa8d52bee0d42e59761b8cb39f5d47f1b8901aea
patrick36212 <patrick36212@gmail.com>
2024-01-25 17:40:55 +0100

    add media to change paragraph size, add props for contrast, italic, info

--- frontend/src/components/Paragraph/index.js
+++ frontend/src/components/Paragraph/index.js
@@ -1,14 +1,37 @@
 import styled, { css } from "styled-components";
 
 export const Paragraph = styled.p`
-  font-size: ${({ theme }) => theme.font.size.regular}px;
-  font-style: italic;
+  font-size: ${({ theme }) => theme.font.size.small}px;
   margin: 0;
+
+  @media only screen and (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
+    font-size: ${({ theme }) => theme.font.size.regular}px;
+  }
 
   ${({ invalid }) =>
     invalid &&
     css`
       color: ${({ theme }) => theme.colors.error};
       font-weight: ${({ theme }) => theme.font.weight.regular};
+    `}
+
+  ${({ contrast }) =>
+    contrast &&
+    css`
+      color: ${({ theme }) => theme.colors.contrast};
+      font-weight: ${({ theme }) => theme.font.weight.medium};
+      text-transform: uppercase;
+    `}
+    
+    ${({ info }) =>
+    info &&
+    css`
+      text-transform: capitalize;
+    `}
+    
+    ${({ italic }) =>
+    italic &&
+    css`
+      font-style: italic;
     `}
 `;
