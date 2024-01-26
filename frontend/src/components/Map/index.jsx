8c1da84c7a8eac5db8fee2eb6035da03fec4d0a6
patrick36212 <patrick36212@gmail.com>
2024-01-26 22:09:11 +0100

    create CustomMap component with custom hook & react-map-gl pck

--- frontend/src/components/Map/index.jsx
+++ frontend/src/components/Map/index.jsx
@@ -1,58 +1,84 @@
-import mapboxgl from "mapbox-gl";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Map, Marker, Popup } from "react-map-gl";
+import { fitBounds } from "@math.gl/web-mercator";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useEffect } from "react";
+import Img from "../Img/index.jsx";
+import useGetCenterFromBounds from "../../utils/useGetCenterFromBounds.js";
+import { Paragraph } from "../Paragraph/index.js";
 
-const MAPBOX_ACCESS_TOKEN =
-  "pk.eyJ1Ijoia3JhdmllY2RldiIsImEiOiJjbHFxbjBqamEzbTJvMmlvMG41dms1Mm96In0.esw8Ems3QLF6tFeTxtPSOg";
+const TOKEN = import.meta.env.VITE_MAPBOX_GL_TOKEN;
 
-const Map = ({ locations }) => {
+const CustomMap = ({ locations }) => {
+  const mapRef = useRef();
+  const { maxLngLat, minLngLat } = useGetCenterFromBounds(locations);
+
+  const [popupInfo, setPopupInfo] = useState(null);
+  const [viewport, setViewport] = useState({
+    longitude: 0,
+    latitude: 0,
+    zoom: 1,
+  });
+
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/kraviecdev/clqqn5l9400w201pjc4measvn",
-      scrollZoom: false,
-      accessToken: MAPBOX_ACCESS_TOKEN,
+    const newViewport = fitBounds({
+      width: mapRef.current.offsetWidth,
+      height: 300,
+      bounds: [minLngLat, maxLngLat],
     });
 
-    const bounds = new mapboxgl.LngLatBounds();
-
-    locations.forEach((loc) => {
-      const marker = document.createElement("div");
-      marker.className = "marker";
-
-      // Add marker
-      new mapboxgl.Marker({
-        element: marker,
-        anchor: "bottom",
-      })
-        .setLngLat(loc.coordinates)
-        .addTo(map);
-
-      // Add popup
-      new mapboxgl.Popup({
-        offset: 30,
-      })
-        .setLngLat(loc.coordinates)
-        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-        .addTo(map);
-
-      // Extend map bounds to include current location
-      bounds.extend(loc.coordinates);
-    });
-
-    map.fitBounds(bounds, {
-      padding: {
-        top: 200,
-        bottom: 150,
-        left: 100,
-        right: 100,
-      },
-    });
-
-    return () => map.remove();
+    setViewport(newViewport);
   }, []);
 
-  return <div id="map"></div>;
+  const pins = useMemo(
+    () =>
+      locations.map((loc, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={loc.coordinates[0]}
+          latitude={loc.coordinates[1]}
+          anchor="bottom"
+          onClick={(e) => {
+            e.originalEvent.stopPropagation();
+            setPopupInfo(loc);
+          }}
+        >
+          <Img src="/img/pin.png" alt="" />
+        </Marker>
+      )),
+    [],
+  );
+
+  return (
+    <div id="map" ref={mapRef}>
+      <Map
+        {...viewport}
+        padding={{
+          top: 200,
+          bottom: 100,
+        }}
+        onMove={(evt) => setViewport(evt.viewState)}
+        mapStyle="mapbox://styles/kraviecdev/clqqn5l9400w201pjc4measvn"
+        mapboxAccessToken={TOKEN}
+        scrollZoom={false}
+      >
+        {pins}
+
+        {popupInfo && (
+          <Popup
+            anchor="bottom"
+            offset={36}
+            longitude={Number(popupInfo.coordinates[0])}
+            latitude={Number(popupInfo.coordinates[1])}
+            onClose={() => setPopupInfo(null)}
+          >
+            <Paragraph
+              info
+            >{`Day ${popupInfo.day}: ${popupInfo.description}`}</Paragraph>
+          </Popup>
+        )}
+      </Map>
+    </div>
+  );
 };
 
-export default Map;
+export default CustomMap;
