f2cd144e52aab32883d5f394a1a9c8d17df8a98d
patrick36212 <patrick36212@gmail.com>
2024-01-27 16:55:58 +0100

    implement props for cta reformat file

--- frontend/src/components/Img/index.jsx
+++ frontend/src/components/Img/index.jsx
@@ -1,8 +1,24 @@
 import { ImgWrapper, StyledImg } from "./styled.js";
 
-const Img = ({ user, card, backdrop, gallery, shadow, alt, src, children }) => {
+const Img = ({
+  cta,
+  user,
+  card,
+  backdrop,
+  gallery,
+  shadow,
+  alt,
+  src,
+  children,
+}) => {
   return (
-    <ImgWrapper user={user} card={card} backdrop={backdrop} gallery={gallery}>
+    <ImgWrapper
+      cta={cta}
+      user={user}
+      card={card}
+      backdrop={backdrop}
+      gallery={gallery}
+    >
       {shadow && <div className="shadow" />}
       <StyledImg alt={alt} src={src} />
       {children}
