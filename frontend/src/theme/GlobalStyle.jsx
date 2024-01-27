a479b8027c3d1f9872a8d11d2781e1bb27457457
patrick36212 <patrick36212@gmail.com>
2024-01-27 15:03:20 +0100

    add custom scroll bars styles

--- frontend/src/theme/GlobalStyle.jsx
+++ frontend/src/theme/GlobalStyle.jsx
@@ -33,6 +33,30 @@ export const GlobalStyle = createGlobalStyle`
         color: ${({ theme }) => theme.colors.second_font};
     }
 
+    /* ===== Scrollbar CSS ===== */
+    /* Firefox */
+    * {
+        scrollbar-width: thin;
+        scrollbar-color: ${({ theme }) => theme.colors.contrast}
+        ${({ theme }) => theme.colors.main};
+    }
+
+    /* Chrome, Edge, and Safari */
+    *::-webkit-scrollbar {
+        width: 6px;
+        height: 6px;
+    }
+
+    *::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    *::-webkit-scrollbar-thumb {
+        background-clip: padding-box;
+        background-color: ${({ theme }) => theme.colors.contrast};
+        border-radius: 10px;
+    }
+
     header {
         width: 100%;
         background: ${({ theme }) => theme.colors.second};
