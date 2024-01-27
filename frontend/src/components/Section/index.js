1a0d076890e35e59ae684b74cc571c0e741d18cd
patrick36212 <patrick36212@gmail.com>
2024-01-27 15:04:00 +0100

    add review prop and styles it

--- frontend/src/components/Section/index.js
+++ frontend/src/components/Section/index.js
@@ -6,16 +6,26 @@ export const Section = styled.section`
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 16px;
   width: 100%;
-    
-    ${({ images }) =>
-      images &&
-      css`
-        gap: 0;
-        grid-template-columns: 1fr;
 
-        @media only screen and (min-width: ${({ theme }) =>
-            theme.breakpoints.md}px) {
-          grid-template-columns: repeat(3, 1fr);
-        }
-      `}}
+  ${({ images }) =>
+    images &&
+    css`
+      gap: 0;
+      grid-template-columns: 1fr;
+
+      @media only screen and (min-width: ${({ theme }) =>
+          theme.breakpoints.md}px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+    `}
+
+  ${({ reviews }) =>
+    reviews &&
+    css`
+      padding: 12px 6px;
+      grid-auto-flow: column;
+      grid-auto-rows: max-content;
+      overflow: auto hidden;
+      max-height: 230px;
+    `}
 `;
