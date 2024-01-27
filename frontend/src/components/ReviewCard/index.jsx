370fd465e0ad4db44c2fa6b93640071810319522
patrick36212 <patrick36212@gmail.com>
2024-01-27 14:30:07 +0100

    move Review Component to Styled components

--- frontend/src/components/ReviewCard/index.jsx
+++ frontend/src/components/ReviewCard/index.jsx
@@ -1,30 +1,29 @@
+import Img from "../Img/index.jsx";
+import { Wrapper } from "../Wrapper/index.js";
+import { ContextHeading } from "../Title/index.js";
+import { Paragraph } from "../Paragraph/index.js";
+import Icon from "../Icon/Icon.jsx";
+
 const ReviewCard = ({ review }) => {
   const rating = [1, 2, 3, 4, 5];
 
   return (
-    <div className="reviews__card">
-      <div className="reviews__avatar">
-        <img
-          className="reviews__avatar-img"
+    <Wrapper column review>
+      <Wrapper>
+        <Img
+          user
           src={`/img/users/${review.user.photo}`}
           alt={review.user.name}
         />
-        <h6 className="reviews__user">{review.user.name}</h6>
-      </div>
-      <p className="reviews__text">{review.review}</p>
-      <div className="reviews__rating">
+        <ContextHeading>{review.user.name}</ContextHeading>
+      </Wrapper>
+      <Paragraph italic>{review.review}</Paragraph>
+      <Wrapper>
         {rating.map((rate, index) => (
-          <svg
-            key={index}
-            className={`reviews__star reviews__star--${
-              review.rating >= rate ? "active" : "inactive"
-            }`}
-          >
-            <use href="/img/icons.svg#icon-star" />
-          </svg>
+          <Icon rating={review.rating < rate} key={index} name="star" />
         ))}
-      </div>
-    </div>
+      </Wrapper>
+    </Wrapper>
   );
 };
 
