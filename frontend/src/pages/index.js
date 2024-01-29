928ae9e5882aba897f6fde7f5ca29d240f93d922
patrick36212 <patrick36212@gmail.com>
2024-01-29 12:33:27 +0100

    add Error component to exports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from "./Home.jsx";
+export { default as Error } from "./Error.jsx";
 export { default as Overview } from "./Overview.jsx";
 export { default as Tour } from "./Tour.jsx";
 export { default as Login } from "./Login.jsx";
