d2b2138b5e9511e324c3d9a9f906ce7c609061eb
patrick36212 <patrick36212@gmail.com>
2024-02-17 21:48:50 +0100

    import ResetPass & ForgotPass pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,3 +4,5 @@ export { default as Overview } from "./Overview.jsx";
 export { default as Tour } from "./Tour.jsx";
 export { default as Login } from "./Login.jsx";
 export { default as Signup } from "./Signup.jsx";
+export { default as ForgotPass } from "./ForgotPass.jsx";
+export { default as ResetPass } from "./ResetPass.jsx";
