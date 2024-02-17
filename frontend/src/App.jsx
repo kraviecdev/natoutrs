d4e91988950d3048a5191ccb96fc75c1390f8d8d
patrick36212 <patrick36212@gmail.com>
2024-02-17 21:50:13 +0100

    move routes to separate dir/file & and implement exporter router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,14 @@
 import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
 import { theme } from "./theme/theme.jsx";
 import { GlobalStyle } from "./theme/GlobalStyle.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Error, Home, Login, Overview, Signup, Tour } from "./pages/index.js";
-
-import { loader as allToursLoader } from "./pages/Overview.jsx";
-import { loader as tourLoader } from "./pages/Tour.jsx";
-import { loader as user } from "./pages/Home.jsx";
-import { action as loginAction } from "./pages/Login.jsx";
-import { action as signupAction } from "./pages/Signup.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    loader: user,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Overview />,
-        loader: allToursLoader,
-      },
-      {
-        path: "tours/:slug",
-        element: <Tour />,
-        loader: tourLoader,
-      },
-      {
-        path: "login",
-        element: <Login />,
-        action: loginAction,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-        action: signupAction,
-      },
-    ],
-  },
-]);
+import { routes } from "./core/routes.jsx";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <RouterProvider router={routes} />
     </ThemeProvider>
   );
 };
