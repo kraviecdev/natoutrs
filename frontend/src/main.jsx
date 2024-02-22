7fcf3639e89a39ad3df99ab7614672c1bc2f66a7
patrick36212 <patrick36212@gmail.com>
2024-02-22 18:01:18 +0100

    add strict mode to app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <StrictMode>
     <App />
     <ToastContainer position="top-center" />
-  </>,
+  </StrictMode>,
 );
