4f1a658fed069f13d4f64dacafd6a88837a8f7d0
patrick36212 <patrick36212@gmail.com>
2024-02-22 18:02:22 +0100

    change react-context to Outlet context del logout fn

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import Footer from "../components/Footer/index.jsx";
 import Header from "../components/Header/index.jsx";
-import { Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 import customFetch from "../utils/customFetch.js";
-import { toast } from "react-toastify";
-import { createContext, useContext } from "react";
 
 export const loader = async () => {
   try {
@@ -14,25 +12,16 @@ export const loader = async () => {
   }
 };
 
-const UserContext = createContext();
 const Home = () => {
   const { data } = useLoaderData();
 
-  const logout = async () => {
-    await customFetch.get("/users/logout");
-    toast.success("Logged out.");
-    redirect("/");
-    window.location.reload();
-  };
-
   return (
-    <UserContext.Provider value={data}>
-      <Header user={data} logout={logout} />
-      <Outlet />
+    <>
+      <Header data={data} />
+      <Outlet context={data} />
       <Footer />
-    </UserContext.Provider>
+    </>
   );
 };
 
-export const useUserContext = () => useContext(UserContext);
 export default Home;
