12d645a43a68e0e97357082649aced9d82383850
patrick36212 <patrick36212@gmail.com>
2024-02-22 13:44:14 +0100

    implement actions and ChangePass route

--- frontend/src/core/routes.jsx
+++ frontend/src/core/routes.jsx
@@ -10,15 +10,19 @@ import {
   Tour,
   Account,
   Settings,
+  ChangePass,
 } from "../pages/index.js";
 
 import { loader as allToursLoader } from "../pages/Overview.jsx";
 import { loader as tourLoader } from "../pages/Tour.jsx";
+import { loader as userLoader } from "../pages/Home.jsx";
+
 import { action as loginAction } from "../pages/Login.jsx";
 import { action as signupAction } from "../pages/Signup.jsx";
 import { action as forgotPass } from "../pages/ForgotPass.jsx";
 import { action as resetPass } from "../pages/ResetPass.jsx";
-import { loader as userLoader } from "../pages/Home.jsx";
+import { action as updateUserData } from "../pages/Settings.jsx";
+import { action as updateUserPass } from "../pages/ChangePass.jsx";
 
 export const routes = createBrowserRouter([
   {
@@ -64,6 +68,12 @@ export const routes = createBrowserRouter([
           {
             index: true,
             element: <Settings />,
+            action: updateUserData,
+          },
+          {
+            path: "pass-change",
+            element: <ChangePass />,
+            action: updateUserPass,
           },
         ],
       },
