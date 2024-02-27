986bec0026e10270b3a1bc14c9c89fdc410e44d6
patrick36212 <patrick36212@gmail.com>
2024-02-27 17:16:45 +0100

    Change Card component to TourCard & simplify it

---
+++ frontend/src/components/TourCard/index.jsx
@@ -0,0 +1,55 @@
+import { ContextHeading, SecondaryHeading } from "../common/Title/index.js";
+import { StyledLink } from "../common/Link/index.js";
+import { Paragraph } from "../common/Paragraph/index.js";
+import { Article } from "../common/Article/index.js";
+import { Wrapper } from "../common/Wrapper/index.js";
+import { Section } from "../common/Section/index.js";
+import Img from "../common/Img/index.jsx";
+import IconInfo from "../common/IconInfo/index.jsx";
+
+const TourCard = ({ tour }) => {
+  const date = new Date(tour.startDates[0]);
+  const tourDate = date.toLocaleString("en-us", {
+    month: "short",
+    year: "numeric",
+  });
+
+  return (
+    <Article>
+      <Img $card src={`img/tours/${tour.imageCover}`} alt={tour.name}>
+        <Wrapper>
+          <SecondaryHeading $card>{tour.name}</SecondaryHeading>
+        </Wrapper>
+      </Img>
+
+      <Wrapper $column $card>
+        <ContextHeading>
+          tour difficulty <span>{tour.difficulty}</span>
+        </ContextHeading>
+        <Paragraph $italic>{tour.summary}</Paragraph>
+        <Section $inner>
+          <IconInfo name="clock" info={`${tour.duration} days`} />
+          <IconInfo name="flag" info={`${tour.locations.length} stops`} />
+          <IconInfo name="calendar" info={tourDate} />
+          <IconInfo name="user" info={`${tour.maxGroupSize} people`} />
+        </Section>
+      </Wrapper>
+
+      <Wrapper $card $secondary>
+        <Section $inner>
+          <Paragraph>
+            <span>${tour.price} </span>per person
+          </Paragraph>
+          <Paragraph>
+            <span>{tour.ratingsAverage} </span>rating ({tour.ratingsQuantity})
+          </Paragraph>
+        </Section>
+        <StyledLink $cta to={`/tours/${tour.slug}`}>
+          Details
+        </StyledLink>
+      </Wrapper>
+    </Article>
+  );
+};
+
+export default TourCard;
