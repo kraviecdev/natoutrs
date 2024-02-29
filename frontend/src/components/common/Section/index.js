eecd0ec00443548e09dbe637c3e23627e941ecbc
patrick36212 <patrick36212@gmail.com>
2024-02-29 19:54:44 +0100

    add about section prop, change cta variant

--- frontend/src/components/common/Section/index.js
+++ frontend/src/components/common/Section/index.js
@@ -3,17 +3,29 @@ import styled, { css } from "styled-components";
 export const Section = styled.section`
   display: grid;
   margin: 0 auto;
-  grid-template-columns: 1fr;
-  gap: 16px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
   width: 100%;
 
-  @media only screen and (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
+  ${(props) =>
+    props.$about &&
+    css`
+      justify-items: start;
+      align-items: baseline;
+    `}
+
+  ${(props) =>
+    props.$inner &&
+    css`
+      justify-items: start;
+      grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+      gap: 10px;
+    `}
 
   ${(props) =>
     props.$images &&
     css`
+      grid-template-columns: 1fr;
       gap: 0;
 
       @media only screen and (min-width: ${({ theme }) =>
@@ -25,6 +37,7 @@ export const Section = styled.section`
   ${(props) =>
     props.$reviews &&
     css`
+      justify-items: start;
       padding: 12px 6px;
       grid-template-columns: none;
       grid-auto-flow: column;
@@ -39,7 +52,8 @@ export const Section = styled.section`
     css`
       padding: 24px 10px 8px;
       grid-template-columns: 1fr;
-      border-radius: 24px;
+      border-radius: 12px;
+      background: ${({ theme }) => theme.colors.second};
       box-shadow: 0 4px 10px ${({ theme }) => theme.colors.shadow};
 
       @media only screen and (min-width: ${({ theme }) =>
