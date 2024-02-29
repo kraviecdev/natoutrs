34c056db46ba460ba3c42a15d597716caa94b825
patrick36212 <patrick36212@gmail.com>
2024-02-29 19:37:08 +0100

    add card prop for positioning h3 component, add bold contrast to all spans inside h components

--- frontend/src/components/common/Title/index.js
+++ frontend/src/components/common/Title/index.js
@@ -4,9 +4,14 @@ const common = css`
   text-transform: uppercase;
   text-shadow: 4px 4px 6px ${({ theme }) => theme.colors.shadow};
   margin: 0;
+
+  span {
+    font-weight: ${({ theme }) => theme.font.weight.bold};
+    color: ${({ theme }) => theme.colors.contrast};
+  }
 `;
 export const MainHeading = styled.h1`
-  color: ${({ theme }) => theme.colors.second_font};
+  color: ${({ theme }) => theme.colors.contrast};
   font-size: ${({ theme }) => theme.font.size.medium}px;
 
   @media only screen and (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
@@ -27,6 +32,16 @@ export const SecondaryHeading = styled.h3`
   @media only screen and (min-width: ${({ theme }) => theme.breakpoints.sm}px) {
     font-size: ${({ theme }) => theme.font.size.extraMedium}px;
   }
+
+  ${(props) =>
+    props.$card &&
+    css`
+      position: inherit;
+      right: 10px;
+      bottom: 10%;
+      max-width: 60%;
+      text-align: right;
+    `}
 
   ${common}
 `;
