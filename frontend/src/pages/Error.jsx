1845f26c871c2ec433a99ef178b0464e08f5d916
patrick36212 <patrick36212@gmail.com>
2024-03-03 19:34:08 +0100

    Simplify 404 page

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,18 +1,15 @@
-import { Wrapper } from "../components/Wrapper/index.js";
-import { StyledLink } from "../components/Link/index.js";
-import Img from "../components/Img/index.jsx";
-import { Main } from "../components/Main/index.js";
+import { StyledLink } from "../components/common/Link/index.js";
+import { Main } from "../components/common/Main/index.js";
+import Img from "../components/common/Img/index.jsx";
 
 const Error = () => {
   return (
-    <Main $column>
-      <Wrapper $error column$>
-        <Img $card src="/img/error.png" alt="404 Image">
-          <StyledLink $contrast to="/">
-            back home
-          </StyledLink>
-        </Img>
-      </Wrapper>
+    <Main>
+      <Img $error src="/img/error.png" alt="404 Image">
+        <StyledLink $cta $error to="/">
+          back home
+        </StyledLink>
+      </Img>
     </Main>
   );
 };
