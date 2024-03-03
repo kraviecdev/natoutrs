c9a5c6bee4abd31ac5d4dc15efa9152dc204d15f
patrick36212 <patrick36212@gmail.com>
2024-03-03 19:35:42 +0100

    change dvw for 100% to nov be x-overflowed

--- frontend/src/components/common/Backdrop/styled.js
+++ frontend/src/components/common/Backdrop/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BackdropBackground = styled.div`
   position: relative;
-  width: 100dvw;
+  width: 100%;
   display: flex;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.second};
