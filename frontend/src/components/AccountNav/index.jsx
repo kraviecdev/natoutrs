f0f84fa7df87d42eb9d9ac8ccd46bbc230f37746
patrick36212 <patrick36212@gmail.com>
2024-03-04 18:41:39 +0100

    create AccountNav function component for Account Page

---
+++ frontend/src/components/AccountNav/index.jsx
@@ -0,0 +1,78 @@
+import { Nav } from "../common/Nav/index.js";
+import Burger from "../common/Burger/index.jsx";
+import { StyledLink } from "../common/Link/index.js";
+import IconInfo from "../common/IconInfo/index.jsx";
+import { useState } from "react";
+
+const AccountNav = ({ data }) => {
+  const [active, setActive] = useState(false);
+  const user = [
+    {
+      icon: "settings",
+      info: "settings",
+      to: "settings",
+    },
+    {
+      icon: "lock",
+      info: "Password",
+      to: "pass-change",
+    },
+    {
+      icon: "briefcase",
+      info: "My bookings",
+      to: "",
+    },
+    {
+      icon: "star",
+      info: "My reviews",
+      to: "reviews",
+    },
+    {
+      icon: "credit-card",
+      info: "Billing",
+      to: "",
+    },
+  ];
+  const admin = [
+    {
+      icon: "map",
+      info: "Manage tours",
+      to: "menage-tours",
+    },
+    {
+      icon: "users",
+      info: "menage users",
+      to: "menage-users",
+    },
+    {
+      icon: "star",
+      info: "menage reviews",
+      to: "menage-reviews",
+    },
+    {
+      icon: "briefcase",
+      info: "menage bookings",
+      to: "menage-bookings",
+    },
+  ];
+
+  return (
+    <Nav $settings $active={active}>
+      <Burger $active={active} onClick={() => setActive(!active)} />
+      {user &&
+        user.map((anchor, index) => (
+          <StyledLink key={index} to={anchor.to}>
+            <IconInfo name={anchor.icon} info={anchor.info} $contrast />
+          </StyledLink>
+        ))}
+      {data?.role === "admin" &&
+        admin.map((anchor, index) => (
+          <StyledLink key={index} to={anchor.to}>
+            <IconInfo name={anchor.icon} info={anchor.info} $contast />
+          </StyledLink>
+        ))}
+    </Nav>
+  );
+};
+
+export default AccountNav;
