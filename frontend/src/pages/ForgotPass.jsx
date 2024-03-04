e386ab14db6c389bf99f5907ca54be16f8ba4d73
patrick36212 <patrick36212@gmail.com>
2024-03-04 17:03:28 +0100

    implement PageForm into new ResetPass version

--- frontend/src/pages/ForgotPass.jsx
+++ frontend/src/pages/ForgotPass.jsx
@@ -1,23 +1,17 @@
 import { redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch.js";
-import useValidator from "../utils/useValidator.js";
-import { Main } from "../components/Main/index.js";
-import { SecondaryHeading } from "../components/Title/index.js";
-import { FormButton, StyledForm } from "../components/Form/index.js";
-import FormRow from "../components/FormRow/index.jsx";
 import { toast } from "react-toastify";
-import { Paragraph } from "../components/Paragraph/index.js";
-import { Wrapper } from "../components/Wrapper/index.js";
-import { StyledLink } from "../components/Link/index.js";
+import { Paragraph } from "../components/common/Paragraph/index.js";
+import { Wrapper } from "../components/common/Wrapper/index.js";
+import { StyledLink } from "../components/common/Link/index.js";
+import PageForm from "../components/PageForm/index.jsx";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   try {
     await customFetch.post("/users/forgot-pass", data);
-    toast.success(
-      "If email exists in our database, you will receive message with link to password change",
-    );
+    toast.success("If email exists in Natours, you will get a message.");
     return redirect("/");
   } catch (error) {
     toast.error(error?.response?.data?.message);
@@ -39,45 +33,20 @@ const ForgotPass = () => {
     },
   ];
 
-  const { data, handleChange } = useValidator(initialState);
-
   return (
-    <Main $column>
-      <StyledForm method="post">
-        <SecondaryHeading>Enter your email</SecondaryHeading>
-
-        {data &&
-          data.map((field, index) => (
-            <FormRow
-              key={index}
-              as={field.as}
-              placeholder={field.placeholder}
-              type={field.type}
-              label={field.label}
-              name={field.name}
-              value={field.value || ""}
-              onChange={(event) => handleChange(field.name, event)}
-              $invalid={!field.validation}
-            />
-          ))}
-
-        <FormButton
-          disabled={data.some(
-            (field) => field.validation === false || field.value === "",
-          )}
-          type="submit"
-        >
-          Send
-        </FormButton>
-
-        <Wrapper $center>
-          <Paragraph>Not a member yet?</Paragraph>
-          <StyledLink $contrast to="/signup">
-            Sign up
-          </StyledLink>
-        </Wrapper>
-      </StyledForm>
-    </Main>
+    <PageForm
+      method="post"
+      initialState={initialState}
+      heading="Enter your email"
+      button="Send"
+    >
+      <Wrapper $center>
+        <Paragraph>Not a member yet?</Paragraph>
+        <StyledLink $contrast to="/signup">
+          Sign up
+        </StyledLink>
+      </Wrapper>
+    </PageForm>
   );
 };
 
