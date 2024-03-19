f90fc3828f0e6e4c9089487956eec35089286a26
patrick36212 <patrick36212@gmail.com>
2024-03-19 21:09:53 +0100

    add ternary operator for DragDrop and FormRow

--- frontend/src/components/PageForm/index.jsx
+++ frontend/src/components/PageForm/index.jsx
@@ -2,27 +2,49 @@ import { FormButton, StyledForm } from "../common/Form/index.js";
 import { SecondaryHeading } from "../common/Title/index.js";
 import FormRow from "../common/FormRow/index.jsx";
 import useValidator from "../../utils/useValidator.js";
+import DragDrop from "../common/DragDrop/index.jsx";
 
-const PageForm = ({ method, initialState, heading, button, children }) => {
-  const { data, handleChange } = useValidator(initialState);
+const PageForm = ({
+  method,
+  encType,
+  initialState,
+  heading,
+  button,
+  children,
+}) => {
+  const { data, handleDrop, handleChange } = useValidator(initialState);
 
   return (
-    <StyledForm method={method}>
+    <StyledForm method={method} encType={encType}>
       <SecondaryHeading>{heading}</SecondaryHeading>
       {data &&
-        data.map((row, index) => (
-          <FormRow
-            key={index}
-            label={row.label}
-            as={row.as}
-            value={row.value || ""}
-            onChange={(event) => handleChange(row.name, event)}
-            placeholder={row.placeholder}
-            name={row.name}
-            type={row.type}
-            $invalid={!row.validation}
-          />
-        ))}
+        data.map((row, index) =>
+          row.file ? (
+            <DragDrop
+              key={index}
+              src={row.src}
+              alt={row.alt}
+              handleDrop={(event) => handleDrop(event)}
+              onChange={(event) => handleChange(row.name, event)}
+              name={row.name}
+              type={row.type}
+              accept={row.accept}
+              multiple={row.multiple}
+            />
+          ) : (
+            <FormRow
+              key={index}
+              label={row.label}
+              as={row.as}
+              value={row.value || ""}
+              onChange={(event) => handleChange(row.name, event)}
+              placeholder={row.placeholder}
+              name={row.name}
+              type={row.type}
+              $invalid={!row.validation}
+            />
+          ),
+        )}
       <FormButton
         disabled={data.some(
           (field) => field.validation === false || field.value === "",
