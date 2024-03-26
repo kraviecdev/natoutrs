e778260cddea0e01bb996567b3385bd636a4fa31
patrick36212 <patrick36212@gmail.com>
2024-03-26 17:33:22 +0100

    specify routes for managing Tours, Users & Review + delete tour with only action

--- frontend/src/core/routes.jsx
+++ frontend/src/core/routes.jsx
@@ -15,6 +15,9 @@ import {
   AllReviews,
   AllTours,
   AllUsers,
+  ManageUser,
+  ManageReview,
+  ManageTour,
 } from "../pages/index.js";
 
 import { loader as toursLoader } from "../pages/Overview.jsx";
@@ -24,6 +27,9 @@ import { loader as myReviewsLoader } from "../pages/MyReviews.jsx";
 import { loader as allReviewsLoader } from "../pages/AllReviews.jsx";
 import { loader as allToursLoader } from "../pages/AllTours.jsx";
 import { loader as allUsersLoader } from "../pages/AllUsers.jsx";
+import { loader as manageReviewLoader } from "../pages/ManageReview.jsx";
+import { loader as manageTourLoader } from "../pages/ManageTour.jsx";
+import { loader as manageUserLoader } from "../pages/ManageUser.jsx";
 
 import { action as loginAction } from "../pages/Login.jsx";
 import { action as signupAction } from "../pages/Signup.jsx";
@@ -31,6 +37,10 @@ import { action as forgotPass } from "../pages/ForgotPass.jsx";
 import { action as resetPass } from "../pages/ResetPass.jsx";
 import { action as updateUserData } from "../pages/Settings.jsx";
 import { action as updateUserPass } from "../pages/ChangePass.jsx";
+import { action as updateReview } from "../pages/ManageReview.jsx";
+import { action as updateTour } from "../pages/ManageTour.jsx";
+import { action as updateUser } from "../pages/ManageUser.jsx";
+import { action as deleteAction } from "../pages/Delete.jsx";
 
 export const routes = createBrowserRouter([
   {
@@ -70,11 +80,11 @@ export const routes = createBrowserRouter([
         action: resetPass,
       },
       {
-        path: "me",
+        path: "settings",
         element: <Account />,
         children: [
           {
-            path: "settings",
+            index: true,
             element: <Settings />,
             action: updateUserData,
           },
@@ -102,6 +112,28 @@ export const routes = createBrowserRouter([
             path: "manage-users",
             element: <AllUsers />,
             loader: allUsersLoader,
+          },
+          {
+            path: "manage-review/:id",
+            element: <ManageReview />,
+            loader: manageReviewLoader,
+            action: updateReview,
+          },
+          {
+            path: "manage-tour/:id",
+            element: <ManageTour />,
+            loader: manageTourLoader,
+            action: updateTour,
+          },
+          {
+            path: "manage-user/:id",
+            element: <ManageUser />,
+            loader: manageUserLoader,
+            action: updateUser,
+          },
+          {
+            path: ":pathname/delete/:id",
+            action: deleteAction,
           },
         ],
       },
