88494bac425c3205f0f673e2a7a6a4ca35003e77
patrick36212 <patrick36212@gmail.com>
2024-03-29 13:58:42 +0100

    implement new form version with standard html validation

--- frontend/src/components/PageForm/index.jsx
+++ frontend/src/components/PageForm/index.jsx
@@ -1,63 +1,46 @@
 import { FormButton, StyledForm } from "../common/Form/index.js";
 import { SecondaryHeading } from "../common/Title/index.js";
 import FormRow from "../common/FormRow/index.jsx";
-import useValidator from "../../utils/useValidator.js";
 import DragDrop from "../common/DragDrop/index.jsx";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 
 const PageForm = ({
   method,
   encType,
+  $second,
   initialState,
   heading,
   button,
   children,
 }) => {
-  const { data, handleChange } = useValidator(initialState);
+  const formRef = useRef();
   const [disabled, setDisabled] = useState(false);
 
-  useEffect(() => {
-    if (data.some((obj) => obj.required === true)) {
-      if (data.some((obj) => obj.value === "" || obj.validation === false)) {
-        setDisabled(true);
-      } else {
-        setDisabled(false);
-      }
-    }
-  }, [data]);
+  const onFormChange = () => {
+    const form = formRef.current;
+
+    const anyInvalid = Array.from(form.elements).some((element) => {
+      return element.nodeName === "INPUT" && !element.validity.valid;
+    });
+
+    anyInvalid ? setDisabled(true) : setDisabled(false);
+  };
 
   return (
-    <StyledForm method={method} encType={encType}>
-      <SecondaryHeading>{heading}</SecondaryHeading>
-      {data &&
-        data.map((row, index) =>
-          row.file ? (
-            <DragDrop
-              key={index}
-              src={row.src}
-              alt={row.alt}
-              onChange={(event) => handleChange(row.name, event)}
-              name={row.name}
-              type={row.type}
-              accept={row.accept}
-              multiple={row.multiple}
-            />
+    <StyledForm
+      ref={formRef}
+      onChange={onFormChange}
+      method={method}
+      encType={encType}
+      $second={$second}
+    >
+      {heading && <SecondaryHeading>{heading}</SecondaryHeading>}
+      {initialState &&
+        initialState.map((row, index) =>
+          row.props.type === "file" ? (
+            <DragDrop key={index} row={row} />
           ) : (
-            <FormRow
-              key={index}
-              label={row.label}
-              as={row.as}
-              value={row.value || ""}
-              onChange={(event) => handleChange(row.name, event)}
-              placeholder={row.placeholder}
-              name={row.name}
-              type={row.type}
-              $invalid={!row.validation}
-              required={row.required}
-              message={!row.validation && row.message}
-              select={row.select}
-              opt={row.options}
-            />
+            <FormRow key={index} row={row} />
           ),
         )}
       <FormButton disabled={disabled} type="submit">
