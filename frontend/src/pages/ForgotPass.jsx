f730af3b6dfc631998228259c2ecbb7b65fa3b01
patrick36212 <patrick36212@gmail.com>
2024-04-14 19:43:50 +0200

    implement new form, with response from DB

--- frontend/src/pages/ForgotPass.jsx
+++ frontend/src/pages/ForgotPass.jsx
@@ -1,43 +1,45 @@
-import { redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch.js";
 import { toast } from "react-toastify";
 import { StyledLink } from "../components/common/Link/index.js";
 import PageForm from "../components/PageForm/index.jsx";
 import { Main } from "../components/common/Main/index.js";
+import { z } from "zod";
 
-export const action = async ({ request }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  try {
-    await customFetch.post("/users/forgot-pass", data);
-    toast.success("If email exists in Natours, you will get a message.");
-    return redirect("/");
-  } catch (error) {
-    toast.error(error?.response?.data?.message);
-    return error;
-  }
-};
 const ForgotPass = () => {
-  const initialState = [
+  const forgotPassState = [
     {
       name: "email",
-      type: "email",
       label: "Email address",
-      placeholder: "you@example.com",
-      as: "input",
-      value: "",
-      validation: true,
-      regex:
-        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-      required: true,
+      props: {
+        type: "email",
+        placeholder: "you@example.com",
+        as: "input",
+      },
     },
   ];
+
+  const forgotPass = z
+    .object({
+      email: z.string().email(),
+    })
+    .required();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await customFetch.post("/users/forgot-pass", data);
+      return toast.success(response?.data?.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      return error;
+    }
+  };
 
   return (
     <Main>
       <PageForm
-        method="post"
-        initialState={initialState}
+        initialState={forgotPassState}
+        schema={forgotPass}
+        onSubmit={onSubmit}
         heading="Enter your email"
         button="Send"
       >
