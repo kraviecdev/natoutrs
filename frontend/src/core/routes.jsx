bb6e21799c50c1aa058d0cbc91b25cab0069f5b2
patrick36212 <patrick36212@gmail.com>
2024-04-23 18:27:23 +0200

    change loaders to react-query fetching

--- frontend/src/core/routes.jsx
+++ frontend/src/core/routes.jsx
@@ -10,26 +10,19 @@ import {
   Tour,
 } from "../pages/index.js";
 
-import { loader as toursLoader } from "../pages/Overview.jsx";
-import { loader as tourLoader } from "../pages/Tour.jsx";
-import { loader as userLoader } from "../pages/Home.jsx";
-
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    loader: userLoader,
     errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Overview />,
-        loader: toursLoader,
       },
       {
         path: "tours/:slug",
         element: <Tour />,
-        loader: tourLoader,
       },
       {
         path: "login",
