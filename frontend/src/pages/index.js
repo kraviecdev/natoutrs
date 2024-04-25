4eac8168cb57faeaaa7451ce9045366966104883
patrick36212 <patrick36212@gmail.com>
2024-04-25 13:42:43 +0200

    change exports names

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,8 @@
 export { default as Home } from "./Home.jsx";
 export { default as Error } from "./Error.jsx";
-export { default as Overview } from "./Overview.jsx";
-export { default as Tour } from "./Tour.jsx";
-export { default as Login } from "./Login.jsx";
-export { default as Signup } from "./Signup.jsx";
-export { default as ForgotPass } from "./ForgotPass.jsx";
-export { default as ResetPass } from "./ResetPass.jsx";
+export { default as AllTours } from "./tours/AllTours.jsx";
+export { default as TourDetailsPage } from "./tours/TourDetailsPage.jsx";
+export { default as Login } from "./forms/Login.jsx";
+export { default as Signup } from "./forms/Signup.jsx";
+export { default as ForgotPass } from "./forms/ForgotPass.jsx";
+export { default as ResetPass } from "./forms/ResetPass.jsx";
