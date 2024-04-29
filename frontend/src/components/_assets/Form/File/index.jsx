b9859ae8e81175e7b5fb280cf02e9e21162b5d6f
patrick36212 <patrick36212@gmail.com>
2024-04-29 17:34:32 +0200

    add useEffect for changing avatar if has path to it

--- frontend/src/components/_assets/Form/File/index.jsx
+++ frontend/src/components/_assets/Form/File/index.jsx
@@ -1,10 +1,10 @@
 import { InputWrapper, StyledInput, StyledLabel } from "../styled.js";
-import { forwardRef, useRef, useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 import Img from "../../Img/index.jsx";
 import { Paragraph } from "../../Paragraph/index.js";
 
-const File = forwardRef(({ label, errorText, ...params }, ref) => {
-  const [avatar, setAvatar] = useState("img/users/default.jpg");
+const File = forwardRef(({ label, avatarPath, errorText, ...params }, ref) => {
+  const [avatar, setAvatar] = useState("/img/users/default.jpg");
 
   const handleOnChange = (e) => {
     const file = e.target.files[0];
@@ -13,11 +13,17 @@ const File = forwardRef(({ label, errorText, ...params }, ref) => {
     setAvatar(preview);
   };
 
+  useEffect(() => {
+    if (!!avatarPath) {
+      setAvatar(avatarPath);
+    }
+  }, [avatarPath]);
+
   return (
     <InputWrapper $file onChange={handleOnChange}>
-      {avatar && <Img $settings $round src={avatar} />}
+      {avatar && <Img $round $settings src={avatar} />}
       <StyledLabel htmlFor="file">{label}</StyledLabel>
-      <StyledInput ref={ref} {...params} />
+      <StyledInput ref={ref} {...params} id="file" />
       {errorText && (
         <Paragraph $italic $invalid>
           {errorText}
