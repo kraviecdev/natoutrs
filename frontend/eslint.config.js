a02bac0480289b7d034dc608bbbdcac48eeaeb0f
Kraviecdev <patrick36212@gmail.com>
2024-07-31 13:57:56 +0200

    change esling config file

---
+++ frontend/eslint.config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@prettier/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    env: { browser: true, es2020: true },
+    settings: { react: { version: '18.2' } },
+    plugins: ["@typescript-eslint", "prettier", "react-refresh"],
+    files: [
+      "**/*.ts",
+      "**/*.cts",
+      "**.*.mts"
+    ],
+    rules: {
+      "semi": "error",
+      "prefer-const": "error",
+      "prettier/prettier": "error",
+      "spaced-comment": "off",
+      "no-console": "warn",
+      "consistent-return": "off",
+      "func-names": "off",
+      "object-shorthand": "off",
+      "no-process-exit": "off",
+      "no-param-reassign": "off",
+      "no-return-await": "off",
+      "no-underscore-dangle": "off",
+      "class-methods-use-this": "off",
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "prefer-destructuring": ["error", { "object": true, "array": false }],
+      "no-unused-vars": ["error", { "argsIgnorePattern": "req|res|next|val" }]
+    },
+    ignores: [".dist/*"],
+    root: true
+  }
+]
